970d911cae062a214469716e61e90df0630e9d35
Wheatley <30753183+hanlim83@users.noreply.github.com>
2020-01-29 00:12:31 +0800

    Added support for including user in generated logs

--- apache-fake-log-gen.py
+++ apache-fake-log-gen.py
@@ -11,9 +11,11 @@ import argparse
 from faker import Faker
 from random import randrange
 from tzlocal import get_localzone
+
 local = get_localzone()
 
-#todo:
+
+# todo:
 # allow writing different patterns (Common Log, Apache Error log etc)
 # log rotation
 
@@ -32,18 +34,24 @@ class switch(object):
         """Indicate whether or not to enter a case suite"""
         if self.fall or not args:
             return True
-        elif self.value in args: # changed for v1.5, see below
+        elif self.value in args:  # changed for v1.5, see below
             self.fall = True
             return True
         else:
             return False
 
+
 parser = argparse.ArgumentParser(__file__, description="Fake Apache Log Generator")
-parser.add_argument("--output", "-o", dest='output_type', help="Write to a Log file, a gzip file or to STDOUT", choices=['LOG','GZ','CONSOLE'] )
-parser.add_argument("--log-format", "-l", dest='log_format', help="Log format, Common or Extended Log Format ", choices=['CLF','ELF'], default="ELF" )
-parser.add_argument("--num", "-n", dest='num_lines', help="Number of lines to generate (0 for infinite)", type=int, default=1)
+parser.add_argument("--output", "-o", dest='output_type', help="Write to a Log file, a gzip file or to STDOUT",
+                    choices=['LOG', 'GZ', 'CONSOLE'])
+parser.add_argument("--log-format", "-l", dest='log_format', help="Log format, Common or Extended Log Format",
+                    choices=['CLF', 'ELF'], default="ELF")
+parser.add_argument("--num", "-n", dest='num_lines', help="Number of lines to generate (0 for infinite)", type=int,
+                    default=1)
 parser.add_argument("--prefix", "-p", dest='file_prefix', help="Prefix the output file name", type=str)
 parser.add_argument("--sleep", "-s", help="Sleep this long between lines (in seconds)", default=0.0, type=float)
+parser.add_argument("--include-login", "-i", dest='include_login', help="Choose whether to include user names",
+                    choices=['TRUE', 'FALSE'], default="FALSE")
 
 args = parser.parse_args()
 
@@ -51,30 +59,38 @@ log_lines = args.num_lines
 file_prefix = args.file_prefix
 output_type = args.output_type
 log_format = args.log_format
+include_login = args.include_login
 
 faker = Faker()
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
 otime = datetime.datetime.now()
 
-outFileName = 'access_log_'+timestr+'.log' if not file_prefix else file_prefix+'_access_log_'+timestr+'.log'
+outFileName = 'access_log_' + timestr + '.log' if not file_prefix else file_prefix + '_access_log_' + timestr + '.log'
 
 for case in switch(output_type):
     if case('LOG'):
-        f = open(outFileName,'w')
+        f = open(outFileName, 'w')
         break
     if case('GZ'):
-        f = gzip.open(outFileName+'.gz','w')
+        f = gzip.open(outFileName + '.gz', 'w')
         break
     if case('CONSOLE'): pass
     if case():
         f = sys.stdout
 
-response=["200","404","500","301"]
+response = ["200", "404", "500", "301"]
 
-verb=["GET","POST","DELETE","PUT"]
+verb = ["GET", "POST", "DELETE", "PUT"]
 
-resources=["/list","/wp-content","/wp-admin","/explore","/search/tag/list","/app/main/posts","/posts/posts/explore","/apps/cart.jsp?appID="]
+resourcesWL = ["/list", "/wp-content", "/wp-admin", "/explore", "/search/tag/list", "/app/main/posts",
+               "/posts/posts/explore", "/apps/cart.jsp?appID="]
+
+resourcesL = ["/list", "/wp-content", "/wp-admin", "/explore", "/search/tag/list", "/app/main/posts",
+              "/posts/posts/explore", "/apps/cart.jsp?appID=", "/login_success"]
+
+users = ["Elise", "Matthew", "Milton", "Samantha", "Kate", "Natalie", "Crystal", "Thea", "Keith", "Ian", "Lance",
+         "Marcus"]
 
 ualist = [faker.firefox, faker.chrome, faker.safari, faker.internet_explorer, faker.opera]
 
@@ -89,20 +105,32 @@ while (flag):
     ip = faker.ipv4()
     dt = otime.strftime('%d/%b/%Y:%H:%M:%S')
     tz = datetime.datetime.now(local).strftime('%z')
-    vrb = numpy.random.choice(verb,p=[0.6,0.1,0.1,0.2])
+    vrb = numpy.random.choice(verb, p=[0.6, 0.1, 0.1, 0.2])
 
-    uri = random.choice(resources)
-    if uri.find("apps")>0:
-        uri += str(random.randint(1000,10000))
+    if include_login == "FALSE":
+        uri = random.choice(resourcesWL)
+        if uri.find("apps") > 0:
+            uri += str(random.randint(1000, 10000))
+    elif include_login == "TRUE":
+        user = random.choice(users)
+        uri = random.choice(resourcesL)
+        if uri.find("apps") > 0:
+            uri += str(random.randint(1000, 10000))
 
-    resp = numpy.random.choice(response,p=[0.9,0.04,0.02,0.04])
-    byt = int(random.gauss(5000,50))
+    resp = numpy.random.choice(response, p=[0.9, 0.04, 0.02, 0.04])
+    byt = int(random.gauss(5000, 50))
     referer = faker.uri()
-    useragent = numpy.random.choice(ualist,p=[0.5,0.3,0.1,0.05,0.05] )()
-    if log_format == "CLF":
-        f.write('%s - - [%s %s] "%s %s HTTP/1.0" %s %s\n' % (ip,dt,tz,vrb,uri,resp,byt))
-    elif log_format == "ELF": 
-        f.write('%s - - [%s %s] "%s %s HTTP/1.0" %s %s "%s" "%s"\n' % (ip,dt,tz,vrb,uri,resp,byt,referer,useragent))
+    useragent = numpy.random.choice(ualist, p=[0.5, 0.3, 0.1, 0.05, 0.05])()
+    if log_format == "CLF" and include_login == "FALSE":
+        f.write('%s - - [%s %s] "%s %s HTTP/1.0" %s %s\n' % (ip, dt, tz, vrb, uri, resp, byt))
+    elif log_format == "ELF" and include_login == "FALSE":
+        f.write(
+            '%s - - [%s %s] "%s %s HTTP/1.0" %s %s "%s" "%s"\n' % (ip, dt, tz, vrb, uri, resp, byt, referer, useragent))
+    elif log_format == "CLF" and include_login == "TRUE":
+        f.write('%s - %s [%s %s] "%s %s HTTP/1.0" %s %s\n' % (ip, user, dt, tz, vrb, uri, resp, byt))
+    elif log_format == "ELF" and include_login == "TRUE":
+        f.write('%s - %s [%s %s] "%s %s HTTP/1.0" %s %s "%s" "%s"\n' % (
+            ip, user, dt, tz, vrb, uri, resp, byt, referer, useragent))
     f.flush()
 
     log_lines = log_lines - 1
